e26c51f5517f4633d54b6a822adb36308162edd8
TeeConcept <ibiduntosin@yahoo.com>
2022-04-14 06:22:11 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 /**
  * main - test function that prints if integer is zero
- * Return: 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 int i;
 i = 0;
-positive_or_negative(i);
+printf((int i); is zero);
+printf("\n");
+
 return (0);
 }	
