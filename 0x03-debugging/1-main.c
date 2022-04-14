bf85448272facf8408f8980461b834fe49b6655d
TeeConcept <ibiduntosin@yahoo.com>
2022-04-14 07:12:03 -0700

    debug

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
  * main - infinite loop
- * Return: 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int i;
-	printf("infinite loop incoming :(\n");
-	i = 0;
-	printf("infinite loop avoided! \\o/\n");
-	return (0);
+int i;
+printf("Infinite loop incoming :(\n");
+i = 0;
+printf("Infinite loop avoided! \\o/\n");
+return (0);
 }
