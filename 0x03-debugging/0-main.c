6aed9a8a2d0fcfc0d9929e05aff80daf9d3342d0
TeeConcept <ibiduntosin@yahoo.com>
2022-04-14 10:08:34 -0700

    debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,13 +3,10 @@
 * main - Test function that prints if integer is zero
 * Return: Always 0 (Success)
 */
-
 int main(void)
 {
 int i;
-
 i = 0;
-positive_or_negative(i, "\n");
-
+positive_or_negative(i);
 return (0);
-}	
+}
--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,8 +6,8 @@
 int main(void)
 {
 int i;
-printf("Infinite loop incoming :(\n");
 i = 0;
+printf("Infinite loop incoming :(\n");
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
