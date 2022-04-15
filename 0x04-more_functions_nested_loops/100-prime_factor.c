4b9cf0cc7ffcd9d4dca34d546f4bcb1417b4f487
TeeConcept <ibiduntosin@yahoo.com>
2022-04-15 03:32:06 -0700

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,38 +6,36 @@
 
 int main()
 {
-    int i, j, isPrime;
-    long int num;
-    /* Input a number from user */
+int i, j, isPrime;
+long int num;
+/* Input a number from user */
 
-  num = 612852475143;
+num = 612852475143;
 
-    printf("All Prime Factors of %ld are: \n", num);
+printf("All Prime Factors of %ld are: \n", num);
 
-    /* Find all Prime factors */
-    for(i=2; i<=num; i++)
-    {
-        /* Check 'i' for factor of num */
-        if(num%i==0)
-        {
-            /* Check 'i' for Prime */
-            isPrime = 1;
-            for(j=2; j<=i/2; j++)
-            {
-                if(i%j==0)
-                {
-                    isPrime = 0;
-                    break;
-                }
-            }
+/* Find all Prime factors */
+for(i=2; i<=num; i++)
+{
+/* Check 'i' for factor of num */
+if(num%i==0)
+{
+/* Check 'i' for Prime */
+isPrime = 1;
+for(j=2; j<=i/2; j++)
+{
+if(i%j==0)
+{
+isPrime = 0;
+break;
+}
+}
 
-            /* If 'i' is Prime number and factor of num */
-            if(isPrime==1)
-            {
-                printf("%d, ", i);
-            }
-        }
-    }
-
+if(isPrime==1)
+{
+printf("%d, ", i);
+}
+}
+}
     return (0);
 }
