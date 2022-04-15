613f7544447e3bb296e034407246bbfdcb917278
TeeConcept <ibiduntosin@yahoo.com>
2022-04-15 13:52:16 -0700

    integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,7 +3,6 @@
 /**
  * print_number - Function that prints an integer.
  * @n: int type number
- * Description: Can only use _putchar to print.
  */
 void print_number(int n)
 {
@@ -15,8 +14,8 @@ void print_number(int n)
 	/* negatives */
 	if (num < 0)
 	{
-		num *= -1;
-		_putchar('-');
+	num *= -1;
+	_putchar('-');
 	}
 
 	/* count up */
@@ -24,24 +23,24 @@ void print_number(int n)
 	c = 1;
 	while (c)
 	{
-		if (num / (m * 10) > 0)
-			m *= 10;
-		else
-			c = 0;
+	if (num / (m * 10) > 0)
+	m *= 10;
+	else
+	c = 0;
 	}
 
 	/* count down */
 	while (num >= 0)
 	{
-		if (m == 1)
-		{
-			_putchar(num % 10 + '0');
-			num = -1;
-		}
-		else
-		{
-			_putchar((num / m % 10) + '0');
-			m /= 10;
-		}
+	if (m == 1)
+	{
+	_putchar(num % 10 + '0');
+	num = -1;
+	}
+	else
+	{
+	_putchar((num / m % 10) + '0');
+	m /= 10;
+	}
 	}
 }
